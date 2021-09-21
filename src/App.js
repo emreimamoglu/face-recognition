032b0906fa85cffa04d8b8de20e62373b8814dde
Emre <emre.imamoglu@aartimuhendislik.com>
2021-09-21 12:10:19 +0300

    Initial component structure completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-particles-js";
 import "./App.css";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles params={particleOptions} className="particles" />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
