5f35a689b5bc213f1a1d79e1a1dc0f7c55f9e994
Emre <emre.imamoglu@aartimuhendislik.com>
2021-09-22 10:26:00 +0300

    fixed bugs

--- src/services/service.js
+++ src/services/service.js
@@ -2,11 +2,11 @@ const Clarifai = require("clarifai");
 
 export const classifyImage = async (formUrl) => {
   let app = new Clarifai.App({ apiKey: "ae211d8777dd4a66a87f3a7a005619f8" });
-  const res1 = await app.models.predict(
+  const response = await app.models.predict(
     { id: "cfbb105cb8f54907bb8d553d68d9fe20" },
     formUrl
   );
-  const res2 = await res1.outputs[0].data.concepts[0].name;
-  const res3 = await res2;
-  return res3;
+  const responseFromPromise = await response.outputs[0].data.concepts[0].name;
+  const finalResponse = await responseFromPromise;
+  return finalResponse;
 };
